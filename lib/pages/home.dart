c9b22b0e52509d10862a05831b82c08f9c6ddd62
Kaelinator <kirkkael@gmail.com>
2018-08-05 17:29:25 -0500

    cleanup + branding

---
+++ lib/pages/home.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageState createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // int _counter = 0;
+
+  // void _incrementCounter() {
+  //   setState(() {
+  //     _counter++;
+  //   });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        child: const Text('Howdy')
+        // child: const Text('$_counter')
+      )
+    );
+  }
+}
